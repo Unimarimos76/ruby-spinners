e2b5b3384d2f3946d7b2770a337e0ff4d493d2a2
uniuni76 <e155766@ie.u-ryukyu.ac.jp>
2019-10-28 21:52:29 +0900

    add original frame

--- example.rb
+++ example.rb
@@ -1,12 +1,20 @@
 require 'tty-spinner'
 
-
 spinner = TTY::Spinner.new("[:spinner] Loading ...",format: :pulse_2)
 
 spinner.auto_spin 
-
 sleep(2)
-
 spinner.stop('Done!')
 
 
+spinner = TTY::Spinner.new("[:spinner] Loading ...",format: :spin)
+
+spinner.auto_spin 
+sleep(2)
+spinner.stop('Done!')
+
+spinner = TTY::Spinner.new("[:spinner] Loading ...",frames: [".", "o", "0", "@", "*"])
+
+spinner.auto_spin 
+sleep(2)
+spinner.stop('Done!')
